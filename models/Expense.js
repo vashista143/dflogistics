2434a907600a039ea443c57286884b0547bf69d4
vashista <vashista.dara03@gmail.com>
2026-07-08 08:14:40 +0530

    Create Expense.js

---
+++ models/Expense.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    category: {
+      type: String,
+      enum: [
+        "Fuel",
+        "Parking",
+        "Toll",
+        "Maintenance",
+        "Food",
+        "Other",
+      ],
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    location: {
+      type: String,
+      default: "",
+    },
+
+    expenseDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Expense",
+  expenseSchema
+);
