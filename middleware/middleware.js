c3b6e2e5f1449c3d7a8df1e05db8c92b20d1f965
vashista <vashista.dara03@gmail.com>
2026-08-01 01:05:35 +0530

    Update middleware.js

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -15,13 +15,15 @@ const authMiddleware = (req, res, next) => {
   try {
 const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded;
-
+    console.log("authmiddleware for the user", req.user," is done")
     next();
   } catch (error) {
     return res.status(401).json({
       success: false,
       message: "Invalid token",
     });
+        console.log("authmiddleware for the user is failded")
+
   }
 };
 
